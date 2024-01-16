dd11fad0278cddc9b222ca0d19e92b8570952e1a
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-01-16 02:27:01 -0500

    Add files via upload

---
+++ RapidanAPI/README.md
@@ -0,0 +1,1 @@
+RapidanAPI allows Rapidan Energy Group\'s clients to conveniently pull energy data.
---
+++ RapidanAPI/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RapidanAPI',
+    version='0.2',
+    packages=find_packages(),
+    description='RapidanAPI allows Rapidan Energy Group\'s clients to conveniently pull energy data.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'requests',
+        'pandas',
+    ],
+    url='https://github.com/Justin-Diamond/RapidanAPI',
+    author='Justin Diamond',
+    author_email='justin.diamond@rapidanenergy.com'
+)

58859c60f8eb642f8f0cbf627db0d5b36ab3c8da
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-01-16 22:58:20 -0500

    Update energy_calendar.py

--- RapidanAPI/energy_calendar.py
+++ RapidanAPI/energy_calendar.py
@@ -1,8 +1,8 @@
 import requests
 import pandas as pd
 
-def get_ec_data(api_key):
-    url = f"https://rapidan-api-sabineleffler.replit.app/get_ec_data/{api_key}"
+def get_ec_data(rapidan_api_key):
+    url = f"https://rapidan-api-sabineleffler.replit.app/get_ec_data/{rapidan_api_key}"
     response = requests.get(url)
 
     if response.status_code == 200:

284e6ec601b9965232ad2f0195c3db0ac7db35ac
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-05-28 19:09:05 -0400

    Update global_oil_balance.py

--- RapidanAPI/global_oil_balance.py
+++ RapidanAPI/global_oil_balance.py
@@ -1,8 +1,8 @@
 import requests
 import pandas as pd
 
-def get_gob_data(rapidan_api_key, balance_date, columns):
-    url = f"https://rapidan-api-sabineleffler.replit.app/get_gob_data/{rapidan_api_key}/{balance_date}/{columns}"
+def get_gob_data(api_key, balance_id, columns):
+    url = f"https://rapidan-api-sabineleffler.replit.app/global_oil_balance/{api_key}/{balance_id}/{columns}"
     response = requests.get(url)
 
     if response.status_code == 200:

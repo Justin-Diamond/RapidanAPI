cec42c1cde5652788bd5e2f9a0d691cda527fb84
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-05-29 10:39:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RapidanAPI',
-    version='1.0.16',
+    version='1.0.17',
     packages=find_packages(),
     description='RapidanAPI allows Rapidan Energy Group\'s clients to conveniently pull energy data.',
     long_description=open('README_PYPI.md').read(),

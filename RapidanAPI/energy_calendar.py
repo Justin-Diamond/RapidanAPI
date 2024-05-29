07f60d3220bf8de33247e79f8af248358a484356
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-05-28 22:15:16 -0400

    Update energy_calendar.py

--- RapidanAPI/energy_calendar.py
+++ RapidanAPI/energy_calendar.py
@@ -7,19 +7,6 @@ def get_ec_data(api_key):
 
     if response.status_code == 200:
         data = response.json()
-        print(data)  # Print the data to understand its structure
-
-        # Check if the data is a dictionary of scalars
-        if isinstance(data, dict):
-            if all(isinstance(value, (int, float, str)) for value in data.values()):
-                data = [data]  # Convert to a list of records
-
-        try:
-            df = pd.DataFrame(data)
-        except ValueError as e:
-            print(f"Error creating DataFrame: {e}")
-            raise
-
-        return df
+        return pd.DataFrame(data)
     else:
         raise Exception("Failed to fetch data.")

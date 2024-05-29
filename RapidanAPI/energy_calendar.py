e7e71d5c28b22f446e406327247d3134f1a6370a
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-05-29 09:55:21 -0400

    Update energy_calendar.py

--- RapidanAPI/energy_calendar.py
+++ RapidanAPI/energy_calendar.py
@@ -1,9 +1,14 @@
-import httpx
+import pandas as pd
+import numpy as np
+from fastapi import HTTPException
 
-async def energy_calendar(api_key):
-    url = f"https://rapidan-api-sabineleffler.replit.app/calendar/{api_key}"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-    response.raise_for_status()  # Ensure we raise an exception for HTTP errors
-    return response.json()
+async def get_data():
+    csv_file_path = 'calendar.csv'
 
+    try:
+        df = pd.read_csv(csv_file_path)
+        df.replace([np.inf, -np.inf], np.nan, inplace=True)
+        df.dropna(inplace=True)
+        return df
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")

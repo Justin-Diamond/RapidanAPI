f8b79c239c9bbd025808baeee17b8fc338984bff
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-05-29 11:30:46 -0400

    Update utils.py

--- RapidanAPI/utils.py
+++ RapidanAPI/utils.py
@@ -1,27 +1,14 @@
 import json
 from datetime import datetime
 from fastapi import HTTPException
-from replit import web
-from replit.object_storage import Client
-
-client = Client()
 
 def load_api_keys():
-    try:
-        # Download the API keys from Replit's object storage
-        contents = client.download_as_text("api_keys.json")
-        return json.loads(contents)
-    except Exception as e:
-        print(f"Error loading API keys: {e}")
-        return {}
+    with open('api_keys.json', 'r+') as keys_file:
+        return json.load(keys_file)
 
 def update_api_keys(api_keys):
-    try:
-        # Upload the API keys to Replit's object storage
-        contents = json.dumps(api_keys)
-        client.upload_from_text("api_keys.json", contents)
-    except Exception as e:
-        print(f"Error updating API keys: {e}")
+    with open('api_keys.json', 'w') as keys_file:
+        json.dump(api_keys, keys_file)
 
 def check_api_key(key, endpoint):
     api_keys = load_api_keys()
@@ -29,7 +16,7 @@ def check_api_key(key, endpoint):
     if key not in api_keys:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
-    if endpoint.lower() not in (ep.lower() for ep in api_keys[key]['endpoints']):
+    if endpoint not in api_keys[key]['endpoints']:
         raise HTTPException(status_code=403, detail="Forbidden")
 
     if api_keys[key]['count'] >= 1500:

2d0a5a0c338333293ad3804483a1f72865adfaa6
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-05-29 13:43:52 -0400

    Update energy_calendar.py

--- RapidanAPI/energy_calendar.py
+++ RapidanAPI/energy_calendar.py
@@ -17,5 +17,6 @@ async def get_data_async(api_keys, api_key: str):
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="File not found")
 
+# Define a new function name for calling the asynchronous function
 def energy_calendar(api_keys, api_key: str):
     return asyncio.run(get_data_async(api_keys, api_key))

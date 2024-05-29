82a37cb73b0f8d04baa7d928380dbc2491bcba24
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-05-29 10:48:09 -0400

    Update __init__.py

--- RapidanAPI/__init__.py
+++ RapidanAPI/__init__.py
@@ -1,2 +1,3 @@
-from .global_oil_balance import get_data as get_global_oil_balance
-from .energy_calendar import get_data as get_energy_calendar
+from .global_oil_balance import get_data as global_oil_balance
+from .energy_calendar import get_data as energy_calendar
+from .utils import check_api_key, load_api_keys, update_api_keys

594980468f1c33a1aa6c3c35388761dcd80ddb2f
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-05-29 09:55:33 -0400

    Update global_oil_balance.py

--- RapidanAPI/global_oil_balance.py
+++ RapidanAPI/global_oil_balance.py
@@ -1,8 +1,46 @@
-import httpx
+import pandas as pd
+import json
+import os
+from datetime import datetime
+from fastapi import HTTPException
 
-async def global_oil_balance(api_key, balance_id, columns):
-    url = f"https://rapidan-api-sabineleffler.replit.app/global_oil_balance/{api_key}/{balance_id}/{columns}"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-    response.raise_for_status()  # Ensure we raise an exception for HTTP errors
-    return response.json()
+async def get_data(balance_date: str, columns: str):
+    datetime_now = datetime.now()
+
+    if balance_date.lower() == "current":
+        current_year = datetime_now.year % 100
+        current_month = datetime_now.month
+
+        while True:
+            balance_date = f'{current_year}{current_month:02d}'
+            csv_file_path = f'{balance_date}_balance.csv'
+
+            if os.path.isfile(csv_file_path):
+                break
+
+            current_month -= 1
+            if current_month == 0:
+                current_month = 12
+                current_year -= 1
+    else:
+        csv_file_path = f'{balance_date}_balance.csv'
+
+    try:
+        df = pd.read_csv(csv_file_path, index_col="Quarter")
+        df.index.name = f'{df.index.name} ({balance_date})'
+        with open('uniqueIDs.json') as ids:
+            unique_ids = json.load(ids)
+
+        columns = [column.strip().lower() for column in columns.split(',')]
+        if 'all' not in columns:
+            if all(column.lower() in (key.lower() for key in unique_ids) for column in columns):
+                column_names = [unique_ids[key] for key, value in unique_ids.items() if key.lower() in columns]
+                if all(column_name in df.columns for column_name in column_names):
+                    df = df.loc[:, column_names]
+                else:
+                    raise HTTPException(status_code=404, detail="Column not found")
+            else:
+                raise HTTPException(status_code=400, detail="Invalid parameter")
+        return df
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")

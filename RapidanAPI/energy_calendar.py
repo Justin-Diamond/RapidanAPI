918ec4624cd04bbe46d2fcabcfa9c63f48bfa440
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-06-21 13:34:45 -0400

    Update energy_calendar.py

--- RapidanAPI/energy_calendar.py
+++ RapidanAPI/energy_calendar.py
@@ -1,14 +1,7 @@
 import pandas as pd
-import numpy as np
 from fastapi import HTTPException
-import json
-from .utils import check_api_key
 
 def get_data(api_key: str):
-    api_keys = load_api_keys()  # Load API keys
-
-    check_api_key(api_keys, api_key, "energy_calendar")
-
     csv_file_path = 'calendar.csv'
 
     try:
@@ -18,7 +11,3 @@ def get_data(api_key: str):
         return df
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="File not found")
-
-def load_api_keys():
-    with open('api_keys.json') as f:
-        return json.load(f)
